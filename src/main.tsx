7b11d7a3df1a386d825a3b8e7e9e201e96c94af6
Bruno Raposo <brunoraposounb@gmail.com>
2025-07-05 09:49:38 -0300

    fix/theme light

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { GlobalStyle, lightTheme, darkTheme } from './styles/'
+import { GlobalStyle, lightTheme } from './styles/'
 import { ThemeProvider } from 'styled-components'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={lightTheme}>
       <GlobalStyle />
       <App />
     </ThemeProvider>
