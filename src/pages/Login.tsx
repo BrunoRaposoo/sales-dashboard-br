f746d24dffb5b6b0c4f2aa03b3b4d74711f105dd
Bruno Raposo <brunoraposounb@gmail.com>
2025-07-05 09:43:21 -0300

    Feat: Form Component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,11 @@
-import { BannerImage } from '@/components'
+import {
+  BannerImage,
+  FormComponent,
+  Logo,
+  StyledH1,
+  StyledP,
+} from '@/components'
+import { pxToRem } from '@/utils'
 import { Box, Container, Grid } from '@mui/material'
 
 export default function Login() {
@@ -13,7 +20,31 @@ export default function Login() {
             sx={{ alignItems: 'center', display: 'flex', height: '100vh' }}
           >
             <Container>
-              <h1>LOGIN</h1>
+              <Box sx={{ marginBottom: pxToRem(24) }}>
+                <Logo height={41} width={100} />
+              </Box>
+              <Box sx={{ marginBottom: pxToRem(24) }}>
+                <StyledH1>Bem-vindo</StyledH1>
+                <StyledP>Digite sua senha e email para logar</StyledP>
+              </Box>
+              <FormComponent
+                inputs={[
+                  { type: 'email', placeholder: 'E-mail', disabled: true },
+                  { type: 'password', placeholder: 'Senha' },
+                ]}
+                buttons={[
+                  {
+                    className: 'alert',
+                    type: 'submit',
+                    children: 'Login',
+                    disabled: true,
+                  },
+                ]}
+                message={{
+                  msg: 'Sucesso! :)',
+                  type: 'success',
+                }}
+              />
             </Container>
           </Grid>
           <Grid
