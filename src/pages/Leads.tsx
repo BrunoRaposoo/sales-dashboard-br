b89d4bcb99b5bda47c9b016e9bfb8ef9f9ce7397
Bruno Raposo <brunoraposounb@gmail.com>
2025-07-07 17:35:36 -0300

    Feat: Header Component

--- src/pages/Leads.tsx
+++ src/pages/Leads.tsx
@@ -1,6 +1,9 @@
+import { Header } from "@/components"
+
 export default function Leads() {
   return (
     <>
+      <Header />
       <h1>LEADS</h1>
     </>
   )
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,6 +1,9 @@
+import { Header } from "@/components"
+
 export default function Profile() {
   return (
     <>
+      <Header />
       <h1>PROFILE</h1>
     </>
   )
